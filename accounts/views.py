59a81bf8b51fb1d5f521c27daa2df14dbc7329c8
emfpdlzj <redryan@ajou.ac.kr>
2026-03-09 16:34:05 +0900

    feat:내정보 조회 및 수정, 유저 일반 회원가입 및 로그인, 구글 및 카카오 연동

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import MeSerializer
+from rest_framework import status
+
+from .serializers import MeSerializer, MeUpdateSerializer
 
 
 class MeView(APIView):
@@ -9,3 +11,13 @@ class MeView(APIView):
 
     def get(self, request):
         return Response(MeSerializer(request.user).data)
+
+    def patch(self, request):
+        serializer = MeUpdateSerializer(
+            request.user,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(MeSerializer(request.user).data, status=status.HTTP_200_OK)
