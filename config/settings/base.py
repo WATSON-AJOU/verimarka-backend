32bcf727f3c63905182d781ffa8e009978357445
emfpdlzj <redryan@ajou.ac.kr>
2026-03-23 01:09:04 +0900

    refactor: 유저 권한 제한 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -133,7 +133,6 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
-        "accounts.permissions.IsPhoneVerified",
     ),
 }
 
