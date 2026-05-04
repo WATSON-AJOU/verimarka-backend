6cca589d1638299f0dd2e3111a2cf3bcf2d27d90
emfpdlzj <redryan@ajou.ac.kr>
2026-05-04 10:28:27 +0900

    chore: 환경변수 세팅 수정

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -13,10 +13,13 @@ ALLOWED_HOSTS = env.list(
     default=["localhost", "127.0.0.1"],
 )
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOWED_ORIGINS = env.list(
+    "CORS_ALLOWED_ORIGINS",
+    default=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+)
 
 DATABASES = {
     "default": {
